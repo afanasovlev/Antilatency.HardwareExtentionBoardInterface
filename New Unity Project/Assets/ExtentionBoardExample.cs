22bfcf0b34e130a4ce49d88ff06c4ff3dee437ff
afanasovlev <69654381+afanasovlev@users.noreply.github.com>
2020-08-15 20:13:21 +0800

    Update ExtentionBoardExample.cs

--- New Unity Project/Assets/ExtentionBoardExample.cs
+++ New Unity Project/Assets/ExtentionBoardExample.cs
@@ -1,5 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Antilatency.DeviceNetwork;
 using System;
@@ -20,35 +18,18 @@ public class ExtentionBoardExample : MonoBehaviour
     public GameObject obj;
     public float range = 5f;
 
-    public Antilatency.HardwareExtensionInterface.IOutputPin outputPin1;
-    public Antilatency.HardwareExtensionInterface.IOutputPin outputPin2;
-    public Antilatency.HardwareExtensionInterface.IOutputPin outputPin5;
-    public Antilatency.HardwareExtensionInterface.IOutputPin outputPin6;
+    public IOutputPin outputPin1;
+    public IOutputPin outputPin2;
+    public IOutputPin outputPin5;
+    public IOutputPin outputPin6;
 
-    public Antilatency.HardwareExtensionInterface.IOutputPin[] oPins;
-    protected INetwork GetNativeNetwork()
-    {
-        if (network == null)
-        {
-            Debug.LogError("Network is null??????");
-            return null;
-        }
-
-        if (network.NativeNetwork == null)
-        {
-            Debug.LogError("Native network is null");
-            return null;
-        }
-
-        return network.NativeNetwork;
-    }
-
+    public Antilatency.HardwareExtensionInterface.IOutputPin[] oPins = new IOutputPin[4];
+   
 
     void Update()
         {
 
-       
-        inetwork = GetNativeNetwork();
+        inetwork = dLibrary.createNetwork(new[] { new UsbDeviceType { vid = UsbVendorId.Antilatency, pid = 0x0000 } });
 
         if (inetwork == null)
         {
@@ -76,36 +57,32 @@ public class ExtentionBoardExample : MonoBehaviour
         oPins[3] = cotask.createOutputPin(Pins.IO6, PinState.Low);
 
         cotask.run();
-
-
-        float h = Input.GetAxis("Horizontal");
-            float xPos = h * range;
+                            
             float moveSpeed = 3f;
             float turnSpeed = 100f;
-
-            float g = Input.GetAxis("Vertical");
+                   
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
             oPins[1].setState(PinState.High);
             oPins[3].setState(PinState.High);
 
         }
             if (Input.GetKey(KeyCode.DownArrow))
             {
-                obj.transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+            obj.transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
             oPins[0].setState(PinState.High);
             oPins[2].setState(PinState.High);
         }
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                obj.transform.Rotate(Vector3.up * -turnSpeed * Time.deltaTime);
+            obj.transform.Rotate(Vector3.up * -turnSpeed * Time.deltaTime);
             oPins[1].setState(PinState.High);
             oPins[2].setState(PinState.High);
         }
             if (Input.GetKey(KeyCode.RightArrow))
             {
-                obj.transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+            obj.transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
             oPins[0].setState(PinState.High);
             oPins[3].setState(PinState.High);
         }

3b2ab0cc25a98990f105ba22b302c3c96330fb77
afanasovlev <69654381+afanasovlev@users.noreply.github.com>
2020-08-22 08:07:27 +0800

    Update ExtentionBoardExample.cs

--- New Unity Project/Assets/ExtentionBoardExample.cs
+++ New Unity Project/Assets/ExtentionBoardExample.cs
@@ -1,99 +1,143 @@
 using UnityEngine;
-using Antilatency.DeviceNetwork;
+
 using System;
 using Antilatency.HardwareExtensionInterface.Interop;
 using Antilatency.HardwareExtensionInterface;
+using System.Runtime.InteropServices;
+using DLLTest;
 
-public class ExtentionBoardExample : MonoBehaviour
+namespace Antilatency.DeviceNetwork
 {
-    public Antilatency.HardwareExtensionInterface.ILibrary library;
-    public Antilatency.HardwareExtensionInterface.ICotask cotask;
-    public Antilatency.HardwareExtensionInterface.ICotaskConstructor cotaskConstructor;
+    public class ExtentionBoardExample : MonoBehaviour
+    {
 
-    public Antilatency.Integration.DeviceNetwork network;
-    public Antilatency.DeviceNetwork.ILibrary dLibrary;
-    public INetwork inetwork;
-    public NodeHandle nodeHandle;
+        public Antilatency.HardwareExtensionInterface.IOutputPin[] oPins = new IOutputPin[4];
 
-    public GameObject obj;
-    public float range = 5f;
+      
+        public GameObject obj;
+        public float range = 5f;
 
-    public IOutputPin outputPin1;
-    public IOutputPin outputPin2;
-    public IOutputPin outputPin5;
-    public IOutputPin outputPin6;
-
-    public Antilatency.HardwareExtensionInterface.IOutputPin[] oPins = new IOutputPin[4];
-   
-
-    void Update()
+        private void Start()
         {
-        dLibrary = Antilatency.DeviceNetwork.Library.load();
-        inetwork = dLibrary.createNetwork(new[] { new UsbDeviceType { vid = UsbVendorId.Antilatency, pid = 0x0000 } });
 
-        if (inetwork == null)
-        {
-            Debug.LogError("Network is null!!!!");
-            return;
+
         }
-       
-        library = Antilatency.HardwareExtensionInterface.Library.load();
-
-        if (library == null)
+        void Update()
         {
-            Debug.LogError("Failed to create hardware extension interface library");
-            return;
-        }
-        if (dLibrary == null)
-        {
-            Debug.LogError("Failed to create Device network library");
-            return;
-        }
+            
+            var eBoard = new Board();
+            oPins = eBoard.GetOutputPin();
 
-        nodeHandle = new NodeHandle();
-        
-        cotaskConstructor = library.getCotaskConstructor();
-
-
-        cotask = cotaskConstructor.startTask(inetwork, nodeHandle);
-
-        oPins[0] = cotask.createOutputPin(Pins.IO1, PinState.Low);
-        oPins[1] = cotask.createOutputPin(Pins.IO2, PinState.Low);
-        oPins[2] = cotask.createOutputPin(Pins.IO5, PinState.Low);
-        oPins[3] = cotask.createOutputPin(Pins.IO6, PinState.Low);
-
-        cotask.run();
-                            
             float moveSpeed = 3f;
             float turnSpeed = 100f;
-                   
+
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-            oPins[1].setState(PinState.High);
-            oPins[3].setState(PinState.High);
+                oPins[1].setState(PinState.High);
+                oPins[3].setState(PinState.High);
 
-        }
+            }
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 obj.transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
-            oPins[0].setState(PinState.High);
-            oPins[2].setState(PinState.High);
-        }
+                oPins[0].setState(PinState.High);
+                oPins[2].setState(PinState.High);
+            }
             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 obj.transform.Rotate(Vector3.up * -turnSpeed * Time.deltaTime);
-            oPins[1].setState(PinState.High);
-            oPins[2].setState(PinState.High);
-        }
+                oPins[1].setState(PinState.High);
+                oPins[2].setState(PinState.High);
+            }
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 obj.transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
-            oPins[0].setState(PinState.High);
-            oPins[3].setState(PinState.High);
+                oPins[0].setState(PinState.High);
+                oPins[3].setState(PinState.High);
+            }
         }
+
+
+
+    }
+
+    public class Board
+    {
+        public Antilatency.HardwareExtensionInterface.ILibrary library;
+        public Antilatency.HardwareExtensionInterface.ICotask cotask;
+        public Antilatency.HardwareExtensionInterface.ICotaskConstructor cotaskConstructor;
+
+        public Antilatency.Integration.DeviceNetwork network;
+        public Antilatency.DeviceNetwork.ILibrary _library;
+        public INetwork inetwork;
+        public NodeHandle nodeHandle;
+
+
+        public IOutputPin outputPin1;
+        public IOutputPin outputPin2;
+        public IOutputPin outputPin5;
+        public IOutputPin outputPin6;
+
+
+
+        public Board()
+        {
+          
+          
+            _library = Library.load();
+            library = Antilatency.HardwareExtensionInterface.Library.load();
+
+            if (_library == null)
+            {
+                Debug.LogError("Failed to load Antilatency Device Network library");
+                return;
+            }
+
+            if (library == null)
+            {
+                throw new Exception("Hardware extention library is null");
+            }
+
+            _library.setLogLevel(LogLevel.Info);
+            Console.WriteLine("Antilatency Device Network version: " + _library.getVersion());
+
+            cotaskConstructor = library.getCotaskConstructor();
+
+            inetwork = _library.createNetwork(new[] { new UsbDeviceType { vid = UsbVendorId.AntilatencyLegacy, pid = 0x3237 } });
+
+            nodeHandle = new NodeHandle();
+            cotask = cotaskConstructor.startTask(inetwork, nodeHandle);
+
+
         }
 
+        public IOutputPin[] GetOutputPin()
+        {
+            IOutputPin[] oPins = new IOutputPin[4];
+            oPins[0] = cotask.createOutputPin(Pins.IO1, PinState.Low);
+            oPins[1] = cotask.createOutputPin(Pins.IO2, PinState.Low);
+            oPins[2] = cotask.createOutputPin(Pins.IO5, PinState.Low);
+            oPins[3] = cotask.createOutputPin(Pins.IO6, PinState.Low);
 
+            cotask.run();
 
+            return oPins;
+        }
+        public static class Library
+        {
+            [DllImport("AntilatencyDeviceNetwork")]
+            private static extern Antilatency.InterfaceContract.ExceptionCode getLibraryInterface(System.IntPtr unloader, out System.IntPtr result);
+            public static ILibrary load()
+            {
+                System.IntPtr libraryAsIInterfaceIntermediate;
+                getLibraryInterface(System.IntPtr.Zero, out libraryAsIInterfaceIntermediate);
+                Antilatency.InterfaceContract.IInterface libraryAsIInterface = new Antilatency.InterfaceContract.Details.IInterfaceWrapper(libraryAsIInterfaceIntermediate);
+                var library = libraryAsIInterface.QueryInterface<ILibrary>();
+                libraryAsIInterface.Dispose();
+                return library;
+            }
+        }
+    }
 }
+
